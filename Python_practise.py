8affab9f48e058e2c4d5103be8f5cb9bc1cfd1c1
Raghvendra Singh <68850766+Eldrago-Raghvendra@users.noreply.github.com>
2020-10-03 23:52:14 +0530

    Update Python_practise.py

--- Python_practise.py
+++ Python_practise.py
@@ -1,1 +1,25 @@
+my_string=input("Enter the string:")
+str=""
+for i in my_string:
+    str=i+str
+print("Reversed string:",str)
 
+####
+
+rep_string=input("Enter a string:")
+if (len(rep_string)<3):
+    print("Not Possible in this case")
+else:
+    a=len(rep_string)
+    str1=rep_string[:2]
+    str2=rep_string[3:a]
+    print(str1+'h'+str2)
+    
+####
+p=input("Enter first string:")
+q=input("Enter second string:") 
+
+if q in p:
+    print("Its a part of first string")
+else:
+    print("It is not a part of first string")
