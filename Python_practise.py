9307769f00ec27dd1df9fbe1828d8082aafee2ae
Raghvendra Singh <68850766+Eldrago-Raghvendra@users.noreply.github.com>
2020-10-04 23:15:16 +0530

    Update Python_practise.py

--- Python_practise.py
+++ Python_practise.py
@@ -6,14 +6,7 @@ print("Reversed string:",str)
 
 ####
 
-rep_string=input("Enter a string:")
-if (len(rep_string)<3):
-    print("Not Possible in this case")
-else:
-    a=len(rep_string)
-    str1=rep_string[:2]
-    str2=rep_string[3:a]
-    print(str1+'h'+str2)
+
     
 ####
 p=input("Enter first string:")
