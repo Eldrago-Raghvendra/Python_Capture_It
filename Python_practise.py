2f9a7ca19d04d723a121465905abee69a50f3344
Raghvendra Singh <68850766+Eldrago-Raghvendra@users.noreply.github.com>
2020-10-05 23:59:45 +0530

    Update Python_practise.py

--- Python_practise.py
+++ Python_practise.py
@@ -9,10 +9,4 @@ print("Reversed string:",str)
 
     
 ####
-p=input("Enter first string:")
-q=input("Enter second string:") 
 
-if q in p:
-    print("Its a part of first string")
-else:
-    print("It is not a part of first string")
