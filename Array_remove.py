d61128f138d5a0f7feaec16ba403fda6c93d65bd
Raghvendra Singh <68850766+Eldrago-Raghvendra@users.noreply.github.com>
2020-10-24 23:08:27 +0530

    Create Array_remove.py

---
+++ Array_remove.py
@@ -0,0 +1,7 @@
+import array as arr
+a=arr.array('i',[0,9,8,5,3,2,3,4,2])
+print(a)                               #6
+ele=int(input("enter value to be removed:"))
+if ele in a:
+    a.remove(ele)
+print(a)
