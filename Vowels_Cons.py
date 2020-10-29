56eb1893b6445096affdc005b6c6868498cc0575
Raghvendra Singh <68850766+Eldrago-Raghvendra@users.noreply.github.com>
2020-10-29 23:34:23 +0530

    Update Vowels_Cons.py

--- Vowels_Cons.py
+++ Vowels_Cons.py
@@ -1,13 +1,5 @@
 
-#Enter a no. if print if negative, positive or zero 
 
-num = int(input("Enter a number: ")) 
-if num > 0:
-  print("Positive")
-elif num < 0:
-  print("Negative")
-else:
-  print("equal to zero")'''
   
   
   
